5f625bbf5435288d94c19dcf1e4b9ed114e28813
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-03-18 10:29:39 +1000

    [wip] ironing

--- tutw04/tutw04_e1.py
+++ tutw04/tutw04_e1.py
@@ -6,7 +6,7 @@ tamper_weight_tf = 18.2 # ton
 tamper_diameter_m = 1.5 # m
 tamper_height_m = 1.5 # m
 
-# 1 depth of improvement and nc ##########################################
+# 1 Depth of improvement and nc ##########################################
 
 # selected depth of improvement
 Di = 8.2
@@ -16,7 +16,6 @@ nc = 0.35
 
 # message
 print('\n1 Depth of improvement and nc')
-print('---------------------------------------------')
 print(f'Depth of improvement = {Di} m')
 print(f'Coefficient nc       = {nc}')
 
@@ -33,7 +32,38 @@ Hd = Ed_tfm / tamper_weight_tf # m
 
 # message
 print('\n2 Energy per blow and drop height')
-print('---------------------------------------------')
 print(f'Energy per blow = {Ed_tfm:.0f} tf-m')
 print(f'Energy per blow = {Ed:.0f} kJ')
 print(f'Drop height     = {Hd:.1f} m')
+
+# 3 Required total applied energy ########################################
+
+# for landfills, the UAE ranges from 600 to 1100 kJ/m3; taking an average
+UAE_ave =(600 + 1100) / 2.0 # kJ/m3
+
+# total applied energy using the first part of Equation 3.21
+AE_total = UAE_ave * Di # kJ/m2
+
+# message
+print('\n3 Required total applied energy')
+print(f'Average UAE = {UAE_ave} kJ/m³')
+print(f'AE total    = {AE_total:.0f} kJ/m²')
+
+## 4 Applied energy during ironing passes ################################
+
+# assuming that the geomaterial above the landfill is fine grained
+# UAE for semipervious fine-grained soils (average)
+UAE_IP = (250 + 350) / 2.0 # kJ/m3
+
+# assuming that the crater depth is 1.5
+d_cd = 1.5 # m
+
+# compute the required applied energy for ironing passes
+# using again the first part of Equation 3.21,
+# but with UAE_total replaced by UAE_IP and Di replaced by crater depth
+AE_IP = UAE_IP * d_cd # kJ/m2
+
+print(f'\n4 Applied energy during ironing passes')
+print(f'Average UAE during ironing = {UAE_IP} kJ/m³')
+print(f'Crater depth               = {d_cd} m')
+print(f'AE ironing                 = {AE_IP} kJ/m²')
