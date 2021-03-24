9109e8c73a5ab740f8601315bd19a2554fbccb83
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-03-24 17:39:16 +1000

    Simplify e1

--- tutw05/tutw05_e1.py
+++ tutw05/tutw05_e1.py
@@ -10,21 +10,16 @@ REQUIRED
   * subsidence without backfill
 '''
 
-# 0 Input data ###########################################################
-
-h = 5.0 # m
-e_min = 0.456
-e_max = 0.950
-e0 = 0.673
-Dr = 75.0 / 100.0
-
 # 1 Spacing ##############################################################
 
-# using Figure 3.41 to read the influence coefficient IC_total
+# given relative density
+Dr = 75.0 / 100.0
+
+# use Dr in Figure 3.41 to read the influence coefficient IC_total
 IC_total = 12.5
 IC = IC_total / 3.0
 
-# using Figure 3.41 again with IC to read the distance from the vibroflot
+# again in Figure 3.41, use IC to read the distance from the vibroflot
 s = 1.7 # m
 
 # message
@@ -33,6 +28,12 @@ print(f'Spacing                s = {s} m')
 
 # 2 Subsidence ###########################################################
 
+# given data
+e_min = 0.456
+e_max = 0.950
+e0 = 0.673
+h = 5.0 # m
+
 # final void ratio
 e1 = e_max - Dr * (e_max - e_min)
 
@@ -40,5 +41,5 @@ e1 = e_max - Dr * (e_max - e_min)
 S = h * (e0 - e1) / (1.0 + e0)
 
 # message
-print(f'Final void ratio         e1 = {e1:.3f}')
+print(f'\nFinal void ratio         e1 = {e1:.3f}')
 print(f'Average ground subsidence S = {S:.3f} m')
