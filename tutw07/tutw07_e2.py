78ddb70fee86614d587a2b123f6e6ed42629aaf4
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-04-07 09:39:03 +1000

    [wip] tut 7

--- tutw07/tutw07_e2.py
+++ tutw07/tutw07_e2.py
@@ -1,52 +1,149 @@
+import numpy as np
+
+'''
+INPUT
+  40 m wideand 1.8 m high embankment (fill)
+  5 m thick soft clay on top of stiffer clay
+  using a 0.3 m thick fill for drainage on the surface
+  groundwater table is 1 m below surface
+FILL
+  fill unit weight = 18 kN/m3
+SOFT CLAY
+  gamma = 15 kN/m3, Es = 1.1 MPa
+  kr = 3.47e-9 m/s, kv = 1.16e-9 m/s
+STONE COLUMNS
+  diameter of 0.8 m and 6 m long
+  square pattern with spacing equal to 2.4 m
+  dry unit weight = 15.7 kN/m3
+  specific gravity = 2.70
+  elastic modulus = 30 MPa
+  Poisson ratio = 0.3
+  columns have 20% clay particles with D10 = 0.005 mm
+OUTPUT
+  Settlement without stone columns
+  Settlement with stone columns
+  Consolidation settlement of composite foundation at one month
+  Assume instantaneous placement of embankment
+'''
+
+# some constants
+gw = 9.81 # kN/m³ unit weight of water
+pi = np.pi # 3.14159...
+sr2 = np.sqrt(2.0)
+sr3 = np.sqrt(3.0)
+
+# function to compute the area replacement ratio
+def calc_area_repl_ratio(dc, s, triangular_pattern = False):
+    C = pi / (2.0 * sr3) if triangular_pattern else pi / 4.0
+    return C * (dc / s) ** 2.0
+
 # 1. Collect fill data #########################################################
 
 # height of embankment and unit weight of embankment
+hf = 1.8 # m
+gf = 18.0 # kN/m³
 
 # additional vertical stress due to fill (immediate) construction
+Dsigz = hf * gf # kPa
+
+# message
+print(f'\n1. Collect fill data')
+print(f'Dsigz = {Dsigz} kPa')
 
 # 2. Collect soft clay data ####################################################
 
 # unit weight (gs), soil modulus (Es), radial permeability (kr), and vertical permeability (kv)
+gs = 15.0 # kN/m³
+Es = 1.1 * 1000.0 # kPa
+kr = 3.47e-9 # m/s
+kv = 1.16e-9 # m/s
 
 # assume Poisson ratio of soft soil (e.g. 0.3)
+nus = 0.3
 
 # height of clay layer
+h = 5.0 # m
 
 # compute coefficient of volume compressibility of natural soil (mvs)
+mvs = (1.0 + nus) * (1.0 - 2.0*nus) / (Es*(1.0 - nus))
 
 # compute coefficient of consolidation due to vertical flow (cv)
+cv = kv / (gw * mvs)
 
 # compute coefficient of consolidation due to radial flow (cr)
+cr = kr / (gw * mvs)
+
+# message
+print(f'\n2. Collect soft clay data')
+print(f'mvs = {mvs:.6f} kPa⁻¹')
+print(f'cv  = {cv:.2e} m²/s')
+print(f'cr  = {cr:.2e} m²/s')
 
 # 3. Collect stone columns data ################################################
 
 # diameter (dc), length (Lc), spacing (s), unit weight (gc), modulus (Ec) and Poisson (nuc)
+dc = 0.8 # m
+Lc = 6.0 # m
+s = 2.4 # m
+gc = 15.7 # kN/m³
+Ec = 30.0 * 1000.0 # kPa
+nuc = 0.3
 
 # specific gravity of column (Gs_col)
+Gs_col = 2.70
 
 # clay particles in column (D10 and P200)
+D10 = 0.005 # mm
+P200 = 20 # %
 
 # porosity of stone column (np)
+np = 1.0 - gc / (gw * Gs_col)
 
 # permeability of stone column (kc)
+kc = 2.19 * (D10**1.479) * (np**6.654) / (P200**0.597)
+
+# message
+print(f'\n3. Collect stone columns data')
+print(f'np = {np:.3f}')
+print(f'kc = {kc:.2e} m/s')
 
 # 4. Compute settlement without stone columns ##################################
 
 # compute S
+S = mvs * Dsigz * h
+print(f'\n4. Compute settlement without stone columns')
+print(f'S = {S:.3f} m')
+print('or')
+print(f'S = {S*1000:.0f} mm')
 
 # 5. Compute stress reduction factor (mu) ######################################
 
 # area replacement ratio
+a_s = calc_area_repl_ratio(dc, s, False)
 
 # modulus ratio of column to soil
+modulus_ratio_calc = Ec / Es
 
 # check modulus ratio (must be smaller than 20)
+modulus_ratio = 20.0 if modulus_ratio_calc > 20.0 else modulus_ratio_calc
 
 # stress concentration ratio
+n_calc = 1.0 + 0.217 * (modulus_ratio - 1.0)
 
 # check stress concentration ratio (must be smaller than 5)
+n = 5.0 if n_calc > 5.0 else n_calc
 
 # compute stress reduction factor (mu)
+mu = 1.0 / (1.0 + a_s * (n - 1.0))
+
+# message
+print(f'\n5. Compute stress reduction factor (mu)')
+print(f'area repl ratio a_s         = {a_s:.3f}')
+print(f'modulus ratio (calc)        = {modulus_ratio_calc:.2f}')
+print(f'modulus ratio (final)       = {modulus_ratio:.2f}')
+print(f'stress conc ratio n (calc)  = {n_calc:.2f}')
+print(f'stress conc ratio n (final) = {n:.2f}')
+print(f'stress reduction factor mu  = {mu:.3f}')
 
 # 6. Settlement with stone columns #############################################
 
