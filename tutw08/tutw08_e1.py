fe2e59cb48a3b6d1f1b9673fae91d77b50da42d5
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-04-08 22:25:58 +1000

    [tutw08] improve

--- tutw08/tutw08_e1.py
+++ tutw08/tutw08_e1.py
@@ -4,7 +4,7 @@ import numpy as np
 pi = np.pi # 3.14159...
 
 # function to calculate the unbonded length
-def anchor_unbonded_length(h, H, chi, theta, psi, strand = False):
+def anchor_unbonded_length(d, H, chi, theta, psi, strand = False):
 
     # minimum overburden depth
     z_min = 4.5 # m
@@ -13,11 +13,11 @@ def anchor_unbonded_length(h, H, chi, theta, psi, strand = False):
     Lub_min = 4.5 if strand else 3.0 # m
 
     # trial horizontal distance of bonded-length-tip
-    x_trial = (H - h + chi / np.cos(psi)) / (np.tan(theta) + np.tan(psi))
+    x_trial = (H - d + chi / np.cos(psi)) / (np.tan(theta) + np.tan(psi))
 
     # minimum horizontal distance of bonded-length-tip
-    if h < z_min:
-        x_min = (z_min - h) / np.tan(theta)
+    if d < z_min:
+        x_min = (z_min - d) / np.tan(theta)
     else:
         x_min = x_trial
 
@@ -41,7 +41,7 @@ def anchor_unbonded_length(h, H, chi, theta, psi, strand = False):
     # results
     return Lub
 
-# 1. Collect input data ##################################################
+# 1. Collect input data #######################################################
 
 # input data
 H = 9.0 # m (excavation depth)
@@ -50,10 +50,13 @@ gamma = 18.0 # kN/m³ (natural unit weight)
 phi_deg = 34.0 # ° (friction angle)
 theta_deg = 15.0 # ° (tendon inclinaton)
 
-# geometric parameters
+# slope of potential slip line
 psi_deg = 45.0 + phi_deg / 2.0
-psi = psi_deg * pi / 180.0
+
+# angles in radians
+phi = phi_deg * pi / 180.0
 theta = theta_deg * pi / 180.0
+psi = psi_deg * pi / 180.0
 
 # calculate chi
 chi = np.max([1.5, H/5]) # m
@@ -68,21 +71,66 @@ print(f'theta = {theta_deg}°')
 print(f'psi   = {psi_deg}°')
 print(f'chi   = {chi} m')
 
-# 2. Calculate unbonded length of anchor # 1 #############################
+# 2. Calculate unbonded length of anchor # 1 ###################################
 
 # depth of head of anchor # 1
-h1 = 3.0
+H1 = 3.0
+d1 = 3.0
 
 # unbonded length of anchor # 1
 print(f'\n2. Calculate unbonded length of anchor # 1')
-Lub1 = anchor_unbonded_length(h1, H, chi, theta, psi)
+Lub1 = anchor_unbonded_length(d1, H, chi, theta, psi)
 
-# 3. Calculate unbonded length of anchor # 2 #############################
+# 3. Calculate unbonded length of anchor # 2 ###################################
 
 # depth of head of anchor # 2
-h2 = 6.0
+H2 = 3.0
+d2 = d1 + H2
 
 # unbonded length of anchor # 2
 print(f'\n3. Calculate unbonded length of anchor # 2')
-Lub2 = anchor_unbonded_length(h2, H, chi, theta, psi)
+Lub2 = anchor_unbonded_length(d2, H, chi, theta, psi)
 
+# 4. Calculate the maximum lateral earth pressure ##############################
+
+# coefficient of the active lateral earth pressure
+Ka = (1.0 - np.sin(phi)) / (1.0 + np.sin(phi))
+
+# total lateral thrust
+Pa = 0.65 * Ka * gamma * H**2.0
+
+# maximum lateral earth pressure
+H3 = 3.0
+p = Pa / (H - H1/3.0 - H3/3.0)
+
+# message
+print(f'\n4. Calculate the maximum lateral earth pressure')
+print(f'Ka = {Ka:.3f}')
+print(f'Pa = {Pa:.1f} kN/m')
+print(f'p  = {p:.1f} kPa')
+
+# 5. Calculate the horizontal anchor loads #####################################
+
+# horizontal anchor load # 1
+Th1 = (2.0*H1/3.0 + H2/2.0) * p
+
+# horizontal anchor load # 2
+Th2 = (H2/2.0 + 23.0*H3/48.0) * p
+
+# message
+print(f'\n5. Calculate the horizontal anchor loads')
+print(f'Th1 = {Th1:.1f} kN/m')
+print(f'Th2 = {Th2:.1f} kN/m')
+
+# 6. Calculate the anchor design loads #########################################
+
+# anchor design load # 1
+M1 = Th1 * Sh / np.cos(theta)
+
+# anchor design load # 2
+M2 = Th2 * Sh / np.cos(theta)
+
+# message
+print(f'\n6. Calculate anchor design loads')
+print(f'M1 = {M1:.1f} kN')
+print(f'M2 = {M2:.1f} kN')
