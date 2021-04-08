a3efd02fdb4a13e5a0febe17b4696b4a5dd1b7a9
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-04-08 19:47:12 +1000

    [wip] tutw08

--- tutw08/tutw08_e1.py
+++ tutw08/tutw08_e1.py
@@ -2,15 +2,17 @@ import numpy as np
 
 # some constants
 pi = np.pi # 3.14159...
+z_min = 4.5 # m, minimum overburden depth
+L_unbonded_min = 4.5 # m, minimum unbonded length
 
 # function to calculate x-distance
-def anchor_calc_x(H, p, chi, theta, psi):
+def anchor_calc_x(p, H, chi, theta, psi):
     return (H + chi / np.cos(psi) - p) / (np.tan(theta) + np.tan(psi))
 
 # 1. Collect input data ##################################################
 
 # input data
-H = 8.0 # m (excavation depth)
+H = 9.0 # m (excavation depth)
 sh = 2.0 # m (horizontal spacing)
 gamma = 18.0 # kN/m³ (natural unit weight)
 phi_deg = 34.0 # ° (friction angle)
@@ -33,3 +35,16 @@ print(f'phi   = {phi_deg}°')
 print(f'theta = {theta_deg}°')
 print(f'psi   = {psi_deg}°')
 print(f'chi   = {chi} m')
+
+# 2. Calculate x for the first anchor ####################################
+
+# depth of head of anchor # 1
+p1 = 3.0
+
+# horizontal distance for anchor # 1
+x1 = anchor_calc_x(p1, H, chi, theta, psi)
+
+# message
+print(f'\n2. Calculate x for the first anchor')
+print(f'p1 = {p1} m')
+print(f'x1 = {x1:.1f} m')
